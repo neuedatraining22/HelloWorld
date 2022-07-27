ca33f73fbad381e0de1ce31707aef2c9a8bea5ef
Matt <matt@multicode.co.uk>
2022-07-27 13:48:06 +0000

    starting strings

---
+++ src/Strings.java
@@ -0,0 +1,13 @@
+import java.util.Locale;
+
+public class Strings {
+
+    public static void main(String[] args) {
+        String myName = "Matt";
+        int length = myName.length();
+        System.out.println("My name is " + myName);
+        System.out.println("it has " + length + " characters");
+        System.out.println(myName.toUpperCase());
+
+    }
+}
